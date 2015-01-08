2059e07d58a8da81556a75d2b5f2ac2de1916768
scty11 <scotty_wright@my.open.ac.uk>
2015-01-08 13:00:53 +0000

    added code

---
+++ MyDataAccessGeneric/MyDataAccessGeneric/DataAccess.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataAccessGeneric
+{
+    class EmployeeDb : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    //allows us to use a person repos to read a employss repos
+    public interface IReadOnlyRepository< out T>
+    {
+        T FindBy(int id);
+        IQueryable<T> FindAll();
+    }
+    //allows us to use a employee repos as a manager repos
+    public interface IWriteOnlyRepository<in T>
+    {
+        void Add(T newEntity);
+        void Delete(T entity);
+        int Commit();
+    }
+    public interface IRepository<T> : IWriteOnlyRepository<T>,IReadOnlyRepository<T>,IDisposable
+    {
+        
+    }
+
+    //the where is constraining the T to be a reference type.
+    //and also it must implement the IEntity interface.
+    //and a default constructor
+    public class SqlRepository<T> : IRepository<T> where T : class, IEntity, new()
+    {
+        DbContext _ctx;
+        DbSet<T> _set;
+
+        public SqlRepository(DbContext ctx)
+        {
+            _ctx = ctx;
+            _set = ctx.Set<T>();
+        }
+
+        public void Add(T newEntity)
+        {
+            if (newEntity.IsValid())
+            {
+                _set.Add(newEntity);
+            }          
+        }
+
+        public void Delete(T entity)
+        {
+            _set.Remove(entity);
+        }
+
+        public T FindBy(int id)
+        {
+            return _set.Find(id);
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return _set;
+        }
+
+        public int Commit()
+        {
+           return  _ctx.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+        }
+    }
+}
---
+++ MyDataAccessGeneric/MyDataAccessGeneric/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataAccessGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<EmployeeDb>());
+            using (IRepository<Employee> emRes 
+                = new SqlRepository<Employee>(new EmployeeDb()))
+            {
+                AddEmployess(emRes);
+                AddManager(emRes);
+                CountEmployees(emRes);
+                FindEmployee(emRes);              
+                DumpPeople(emRes);
+                Console.Read();
+            }
+        }
+
+        //using contravariance here as Manaegr is a employee.
+        private static void AddManager(IWriteOnlyRepository<Manager> emRes)
+        {
+            emRes.Add(new Manager() { Name = "Lee" });
+            emRes.Commit();
+        }
+
+        //using covariance in the generic interface to allow this.
+        private static void DumpPeople(IReadOnlyRepository<Person> emRes)
+        {
+            var employees = emRes.FindAll();
+            foreach (var item in employees)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        private static void FindEmployee(IRepository<Employee> emRes)
+        {
+            var em = emRes.FindBy(1);
+            Console.WriteLine(em.Name);
+        }
+
+        private static void CountEmployees(IRepository<Employee> emRes)
+        {
+            Console.WriteLine(emRes.FindAll().Count());
+        }
+
+        private static void AddEmployess(IRepository<Employee> emRes)
+        {
+            emRes.Add(new Employee { Name = "Scott" });
+            emRes.Add(new Employee { Name = "Lee" });
+            emRes.Commit();
+        }
+
+        
+    }
+}
